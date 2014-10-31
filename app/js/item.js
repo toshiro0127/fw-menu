b37643e9630ad5b359bc6c3eb897d0796603b493
toshiro0127 <t0127t@gmail.com>
2014-10-31 19:52:34 +0900

    商品データを追加

---
+++ app/js/item.js
@@ -0,0 +1,19 @@
+(function(){
+  window.item = {
+  tanrei_green:{
+    name:'端麗グリーンラベル',
+    price:1500,
+    grade:'A',
+  },
+  carori:{
+    name:'カロリ',
+    price:1500,
+    grade:'A',
+  },
+
+
+
+
+
+  };
+})();
