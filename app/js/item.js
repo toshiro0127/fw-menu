ef605e66df6cd5c26d76afe5ef843f28f0245660
toshiro0127 <t0127t@gmail.com>
2014-12-12 20:19:11 +0900

    メニュー表示部分の修正
    メニューをテーブルで表示するようにした。
    ボタンを設置した。

--- app/js/item.js
+++ app/js/item.js
@@ -10,7 +10,31 @@
     price:1500,
     grade:'A',
   },
-
+  coctail_carori:{
+    name:'カクテルカロリ',
+    price:1500,
+    grade:'A',
+  },
+  Asahi_Super_Dry:{
+    name:'アサヒスーパードライ',
+    price:1500,
+    grade:'A',
+  },
+  Nama_beer:{
+    name:'生ビール',
+    price:1500,
+    grade:'A',
+  },
+  Sky_Blue:{
+    name:'スカイブルー',
+    price:1500,
+    grade:'A',
+  },
+  Smirnoff_ICE:{
+    name:'スミノフアイス',
+    price:1500,
+    grade:'A',
+  },
 
 
 
